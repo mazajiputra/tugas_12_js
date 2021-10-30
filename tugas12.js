d82cde8d2879b6fe464b094059208401d3490654
mazajiputra <mazajiputra@gmail.com>
2021-10-31 03:23:00 +0700

    Kumpul Tugas

---
+++ tugas12.js
@@ -0,0 +1,18 @@
+//Original
+var namaBuah=['Pisang','Jeruk','Apel','Mangga'];
+console.log(namaBuah)
+
+//Pop
+function tugas1(data) {   
+    data.pop()
+    return data
+}
+console.log(tugas1(namaBuah))
+
+//Shift
+function tugas2(data) {
+    data.shift()
+    return data
+}
+
+console.log(tugas2(namaBuah))
